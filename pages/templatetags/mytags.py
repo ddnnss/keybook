e831498eeb7dc650c259b1469ca820f05f02ed6e
ddnnss <ddnnss.i1@gmail.com>
2019-12-08 17:06:30 +0300

    add calendar

--- pages/templatetags/mytags.py
+++ pages/templatetags/mytags.py
@@ -6,17 +6,18 @@ register = template.Library()
 
 @register.filter
 def loadjson(data):
-    print(json.loads(data))
-    days = []
-    string = ''
-    for year in json.loads(data):
-        string += f'Год {year} . '
-        print(year)
-        for month in json.loads(data)[year]:
-            print(month)
-            string += f'Месяц {month} Числа: '
-            for day in json.loads(data)[year][month]:
-                string += f'{day} '
-                print (day)
-        days.append(string)
-    return days
+    if data:
+        print(json.loads(data))
+        days = []
+        string = ''
+        for year in json.loads(data):
+            string += f'Год {year} . '
+            print(year)
+            for month in json.loads(data)[year]:
+                print(month)
+                string += f'Месяц {month} Числа: '
+                for day in json.loads(data)[year][month]:
+                    string += f'{day} '
+                    print (day)
+            days.append(string)
+        return days
